01b6411d2024a33af405b5fc906f25c2d6da4702
Antonioangy <dumitruangy26@gmail.com>
2021-11-25 16:02:12 +0100

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,63 @@
+<template>
+   <footer>
+       <div class="center">
+            <div class="signup-social">
+                <button>SIGN-UP NOW!</button>
+            </div>
+            <div class="social-box">
+                <span>FOLLOW US</span>
+                <img src="@/assets/img/footer-facebook.png" alt="#">
+                <img src="@/assets/img/footer-twitter.png" alt="#">
+                <img src="@/assets/img/footer-youtube.png" alt="#">
+                <img src="@/assets/img/footer-pinterest.png" alt="#">
+                <img src="@/assets/img/footer-periscope.png" alt="#">
+            </div>
+       </div>
+   </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+footer{
+    display: flex;
+    background-color: #303030;
+    justify-content: space-between;
+    align-items: center;
+    height: 100px;
+    button {
+    background-color: #303030;
+    border: 3px solid #1462ab;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    }
+    .center{
+        display: flex;
+        width: 80%;
+        margin: 0 auto;
+        align-items: center;
+        justify-content: space-between;
+    }
+    span{
+        color: #0689ff;
+        margin: 0 20px;
+    }
+    .social-box{
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+    }
+    img{
+        margin: 0 7px;
+    }
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,32 @@
+<template>
+    <main id="black">
+        <div class="container">
+            <div class="content">Content goes here</div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#black {
+    display: flex;
+    align-items: center;
+    height: 100px;
+    background-color: black;
+    color: white;
+    .container{
+        width: 80%;
+        margin: 0 auto;
+    }
+    .content{
+        float: left;
+        font-size: 25px;
+    }
+}
+</style>
